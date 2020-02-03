a44969084e8f54f2a1e517b01a68673d3e91c28c
Luca <lams3@cin.ufpe.br>
2020-02-03 19:58:15 -0300

    [Done] GGJ2020 project

---
+++ Assets/TheRoadie/Scripts/BackToMenuScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenuScript : MonoBehaviour
+{
+    public Animator animator;
+
+    public void backToMenu()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void onFadeComplete()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+}
---
+++ Assets/TheRoadie/Scripts/PlayerController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Player))]
+public class PlayerController : MonoBehaviour
+{
+
+	public delegate void OnFocusChanged(Interactable newFocus);
+	public OnFocusChanged onFocusChangedCallback;
+
+	public Interactable focus;
+
+	public LayerMask movementMask;
+	public LayerMask interactionMask;
+
+	private Player player;
+	private Camera cam;
+
+	void Start()
+	{
+		player = GetComponent<Player>();
+		cam = Camera.main;
+	}
+
+	void Update()
+	{
+
+		if (EventSystem.current.IsPointerOverGameObject())
+			return;
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, movementMask))
+			{
+				this.player.StopFixing();
+				player.MoveTo(hit.point);
+
+				SetFocus(null);
+			}
+		}
+
+		if (Input.GetMouseButtonDown(1))
+		{
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast(ray, out hit, float.MaxValue, interactionMask))
+			{
+				this.player.StopFixing();
+
+				SetFocus(hit.rigidbody.GetComponent<Interactable>());
+			}
+		}
+
+	}
+
+	void SetFocus(Interactable newFocus)
+	{
+		if (onFocusChangedCallback != null)
+			onFocusChangedCallback.Invoke(newFocus);
+
+		if (focus != newFocus && focus != null)
+		{
+			focus.OnDefocused();
+		}
+
+		focus = newFocus;
+
+		if (focus != null)
+		{
+			focus.OnFocused(transform);
+		}
+	}
+}
